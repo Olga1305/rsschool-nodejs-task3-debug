e50fb0886326515be9e181c95e8e55558d022ea3
Olga Dokukova <dokukova@gmail.com>
2021-05-23 01:24:07 +0200

    fix: compilation bugs

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var app = express();
-var db = require('./db');
+var sequelize = require('./db');
 var user = require('./controllers/usercontroller');
 var game = require('./controllers/gamecontroller')
+const port = process.env.PORT || '4000';
 
-
-db.sync();
+sequelize.sync();
 app.use(require('body-parser'));
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
-app.listen(function() {
-    console.log("App is listening on 4000");
+app.listen(port, function() {
+    console.log(`App is listening on ${port}`);
 })
